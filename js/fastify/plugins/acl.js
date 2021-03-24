2cbc0799dc7fce8f287c033ba68484a2e5e1b497
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-03-24 12:26:52 +0000

    feat: extract middleware to be re-used without being a plugin

--- js/fastify/plugins/acl.js
+++ js/fastify/plugins/acl.js
@@ -2,21 +2,23 @@ import fp from "fastify-plugin";
 
 import { getRipeAuth } from "../../auth";
 
-const acl = async (app, options) => {
-    app.addHook("onRequest", (req, res, next) => {
-        req.getAcl = async ctx => {
-            req.ripeCtx = req.ripeCtx === undefined ? {} : req.ripeCtx;
-            if (options.skipAuth) {
-                req.ripeCtx.user = options.skipUser || "anonymous";
-                return options.skipAcl || ["*"];
-            }
-            const auth = await getRipeAuth(req);
-            req.ripeCtx.user = auth.account.username;
-            return auth.acl.acl;
-        };
-
-        next();
-    });
+const aclPlugin = async (app, options) => {
+    app.addHook("onRequest", acl);
 };
 
-export const ripeAcl = fp(acl);
+const acl = (req, res, next) => {
+    req.getAcl = async ctx => {
+        req.ripeCtx = req.ripeCtx === undefined ? {} : req.ripeCtx;
+        if (options.skipAuth) {
+            req.ripeCtx.user = options.skipUser || "anonymous";
+            return options.skipAcl || ["*"];
+        }
+        const auth = await getRipeAuth(req);
+        req.ripeCtx.user = auth.account.username;
+        return auth.acl.acl;
+    };
+
+    next();
+};
+
+export const ripeAcl = fp(aclPlugin);
