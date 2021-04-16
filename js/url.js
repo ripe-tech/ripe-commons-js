7e6e283b043bac65ea7e09160a2f1a530a6f68b6
Gabriel Candal <gcc@platforme.com>
2021-04-16 17:24:22 +0100

    feat: copy from ripe-util-vue

--- js/url.js
+++ js/url.js
@@ -1,9 +1,21 @@
-export const patchUrl = url => {
+/**
+ * Patches the base URL based on heuristics that try to
+ * make it compatible with the expected base URL format.
+ *
+ * A series of heuristics are used in this patch operation
+ * to make it as "standard" as possible.
+ *
+ * @param {String} url The URL to be patched with a series
+ * of pre-defined heuristics.
+ * @returns {String} The patched URL with a series of values
+ * replaced with proper value.
+ */
+ export const patchBaseUrl = url => {
     const hasProtocol = url.startsWith("http://") || url.startsWith("https://");
-    const isLocalAdress = url.includes("localhost:");
     if (hasProtocol) return url;
-    if (isLocalAdress) return `http://${url}`;
+    const isVercel = url.endsWith(".vercel.app");
+    if (!isVercel) return url;
     return `https://${url}`;
 };
 
-export default patchUrl;
+export default patchBaseUrl;
