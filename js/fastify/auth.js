ce247f7295fa76d92c72ee3ba20763dd3a36518a
João Magalhães <joamag@gmail.com>
2021-05-13 10:54:42 +0100

    style: pretty operation

--- js/fastify/auth.js
+++ js/fastify/auth.js
@@ -1,10 +1,12 @@
-export const verifyKeyFastify = (key, { headerKey = "X-Secret-Key" } = {}) => (req, res, next) => {
-    if (!key) {
-        return next();
-    }
-    const _key = req.query.key || req.headers[headerKey] || null;
-    if (key === _key) {
-        return next();
-    }
-    return next(new Error("Invalid key"));
-};
+export const verifyKeyFastify =
+    (key, { headerKey = "X-Secret-Key" } = {}) =>
+    (req, res, next) => {
+        if (!key) {
+            return next();
+        }
+        const _key = req.query.key || req.headers[headerKey] || null;
+        if (key === _key) {
+            return next();
+        }
+        return next(new Error("Invalid key"));
+    };
