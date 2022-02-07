a5b0a8c8399873e4232c38870d5a9e9236b60de7
João Magalhães <joamag@gmail.com>
2022-02-07 16:33:00 +0000

    feat: improved trimming strategy

--- js/string.js
+++ js/string.js
@@ -55,25 +55,30 @@ const _breakStringByParagraph = value => {
 const _breakStringByWord = (value, maxLength = 80) => {
     const lines = [];
     let line = "";
+
+    // splits the provided value around the space character
+    // allowing proper iteration over the sequence of words
+    // to be able to allocate them over the multiple lines
     value.split(" ").forEach(word => {
         if ((line + word).length > maxLength) {
-            line = line[line.length - 1] === " " ? line.slice(0, -1) : line;
+            line = line.trim();
             lines.push(line);
             line = word;
         } else {
             line += word;
         }
 
+        line = line.trimStart();
         line += " ";
-
-        if (line.length > maxLength) {
-            line = line.slice(0, -1);
-            lines.push(line);
-            line = "";
-        }
     });
+
+    // trims the last line an in case the line is valid
+    // adds it to the complete set of lines
     line = line.trim();
     if (line.length > 0) lines.push(line);
+
+    // returns the final sequence of lines with the complete
+    // set of words properly splitted
     return lines;
 };
 
