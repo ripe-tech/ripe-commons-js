4dbaf7ccac8ca69a7afc1d8f5e4751af2192ae38
Pedro Reis <pedroreis.ad@protonmail.com>
2022-04-20 15:57:03 +0100

    fix: lint fix

--- test/csv.js
+++ test/csv.js
@@ -1,63 +1,111 @@
 const assert = require("assert");
-const ripeCommons = require("..");
+ const mixins = require("../../mixins");
 
-describe("CSV", () => {
-    describe("#parseCsvComplex", () => {
-        it("should allow simple CSV parsing", () => {
-            const result = ripeCommons.parseCsvComplex(
-                "name,title\nJoão Magalhães,mr\nGabriel Candal,mr\n"
-            );
-            assert.deepStrictEqual(result, [
-                ["name", "title"],
-                ["João Magalhães", "mr"],
-                ["Gabriel Candal", "mr"]
-            ]);
-        });
+ describe("csvMixin", () => {
+     describe("#_parseCsv", () => {
+         it("should allow simple CSV parsing", () => {
+             const result = mixins.csvMixin.methods._parseCsv(
+                 "name,title\nJoão Magalhães,mr\nGabriel Candal,mr\n"
+             );
+             assert.deepStrictEqual(result, [
+                 ["name", "title"],
+                 ["João Magalhães", "mr"],
+                 ["Gabriel Candal", "mr"]
+             ]);
+         });
 
-        it("should allow simple CSV object parsing", () => {
-            const result = ripeCommons.parseCsvComplex(
-                "name,title\nJoão Magalhães,mr\nGabriel Candal,mr\n",
-                true
-            );
-            assert.deepStrictEqual(result, [
-                { name: "João Magalhães", title: "mr" },
-                { name: "Gabriel Candal", title: "mr" }
-            ]);
-        });
+         it("should allow simple CSV object parsing", () => {
+             const result = mixins.csvMixin.methods._parseCsv(
+                 "name,title\nJoão Magalhães,mr\nGabriel Candal,mr\n",
+                 true
+             );
+             assert.deepStrictEqual(result, [
+                 { name: "João Magalhães", title: "mr" },
+                 { name: "Gabriel Candal", title: "mr" }
+             ]);
+         });
 
-        it("should allow complex CSV object parsing", () => {
-            const result = ripeCommons.parseCsvComplex(
-                'name,title\n"João,{Maga""lhães}",mr\nGabriel Candal,mr\n',
-                true
-            );
-            assert.deepStrictEqual(result, [
-                { name: 'João,{Maga"lhães}', title: "mr" },
-                { name: "Gabriel Candal", title: "mr" }
-            ]);
-        });
+         it("controls extra elements in line", () => {
+             let result;
 
-        it("should handle extra elements in line", () => {
-            let result;
+             result = mixins.csvMixin.methods._parseCsv(
+                 "name,title\nJoão Magalhães,mr \nGabriel Candal,mr   \n",
+                 true
+             );
+             assert.deepStrictEqual(result, [
+                 { name: "João Magalhães", title: "mr" },
+                 { name: "Gabriel Candal", title: "mr" }
+             ]);
 
-            result = ripeCommons.parseCsvComplex(
-                "name,title\nJoão Magalhães,mr \nGabriel Candal,mr   \n",
-                true
-            );
-            assert.deepStrictEqual(result, [
-                { name: "João Magalhães", title: "mr" },
-                { name: "Gabriel Candal", title: "mr" }
-            ]);
+             result = mixins.csvMixin.methods._parseCsv(
+                 "name,title\nJoão Magalhães,mr \nGabriel Candal,mr   \n",
+                 true,
+                 false
+             );
+             assert.deepStrictEqual(result, [
+                 { name: "João Magalhães", title: "mr " },
+                 { name: "Gabriel Candal", title: "mr   " },
+                 { name: "", title: undefined }
+             ]);
+         });
+     });
 
-            result = ripeCommons.parseCsvComplex(
-                "name,title\nJoão Magalhães,mr \nGabriel Candal,mr   \n",
-                true,
-                false
-            );
-            assert.deepStrictEqual(result, [
-                { name: "João Magalhães", title: "mr " },
-                { name: "Gabriel Candal", title: "mr   " },
-                { name: "", title: undefined }
-            ]);
-        });
-    });
-});
+     describe("#_parseCsvComplex", () => {
+         it("should allow simple CSV parsing", () => {
+             const result = mixins.csvMixin.methods._parseCsvComplex(
+                 "name,title\nJoão Magalhães,mr\nGabriel Candal,mr\n"
+             );
+             assert.deepStrictEqual(result, [
+                 ["name", "title"],
+                 ["João Magalhães", "mr"],
+                 ["Gabriel Candal", "mr"]
+             ]);
+         });
+
+         it("should allow simple CSV object parsing", () => {
+             const result = mixins.csvMixin.methods._parseCsvComplex(
+                 "name,title\nJoão Magalhães,mr\nGabriel Candal,mr\n",
+                 true
+             );
+             assert.deepStrictEqual(result, [
+                 { name: "João Magalhães", title: "mr" },
+                 { name: "Gabriel Candal", title: "mr" }
+             ]);
+         });
+
+         it("should allow complex CSV object parsing", () => {
+             const result = mixins.csvMixin.methods._parseCsvComplex(
+                 'name,title\n"João,{Maga""lhães}",mr\nGabriel Candal,mr\n',
+                 true
+             );
+             assert.deepStrictEqual(result, [
+                 { name: 'João,{Maga"lhães}', title: "mr" },
+                 { name: "Gabriel Candal", title: "mr" }
+             ]);
+         });
+
+         it("controls extra elements in line", () => {
+             let result;
+
+             result = mixins.csvMixin.methods._parseCsvComplex(
+                 "name,title\nJoão Magalhães,mr \nGabriel Candal,mr   \n",
+                 true
+             );
+             assert.deepStrictEqual(result, [
+                 { name: "João Magalhães", title: "mr" },
+                 { name: "Gabriel Candal", title: "mr" }
+             ]);
+
+             result = mixins.csvMixin.methods._parseCsvComplex(
+                 "name,title\nJoão Magalhães,mr \nGabriel Candal,mr   \n",
+                 true,
+                 false
+             );
+             assert.deepStrictEqual(result, [
+                 { name: "João Magalhães", title: "mr " },
+                 { name: "Gabriel Candal", title: "mr   " },
+                 { name: "", title: undefined }
+             ]);
+         });
+     });
+ });
