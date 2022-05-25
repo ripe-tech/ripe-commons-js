54abf347936402242e69eeca2a5c4c8248b0a114
Mauro Benedito <v-mb@platforme.com>
2022-05-25 11:05:08 +0100

    feat: add functions to `env.js`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,6 +19,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 *
 
+## [0.10.2] - 2022-05-23
+
+### Added
+
+* Added `getRipeIdOptions`, `getRipeWhiteAdminOptions` and `getRipeWhiteOptions` retrieval functions
+
 ## [0.10.1] - 2022-05-23
 
 ### Fixed
--- js/env.js
+++ js/env.js
@@ -32,6 +32,25 @@ export const getRipeCoreOptions = (environment, fallback = {}) => {
     }
 };
 
+export const getRipeIdOptions = (environment, fallback) => {
+    switch (environment) {
+        case "now-branch":
+            return {
+                clientId: "3b7f6c17f5047b341f1f79a67c153dc0",
+                clientSecret: "752ec9a665655836540b35bc4e5de5bc7166075f7762256f21ee7e967349712c",
+                redirectUrl: `${window.location.protocol}//${window.location.hostname}/oauth`
+            };
+        case "localhost":
+            return {
+                clientId: "3b7f6c17f5047b341f1f79a67c153dc0",
+                clientSecret: "752ec9a665655836540b35bc4e5de5bc7166075f7762256f21ee7e967349712c",
+                redirectUrl: `http://localhost:${window.location.port}/oauth`
+            };
+        default:
+            return fallback;
+    }
+};
+
 export const getRipePulseOptions = (environment, fallback = {}) => {
     switch (environment) {
         case "ci":
@@ -55,6 +74,64 @@ export const getRipePulseOptions = (environment, fallback = {}) => {
         case "production":
             return {
                 url: "https://pulse.platforme.com/"
+            };
+        default:
+            return fallback;
+    }
+};
+
+export const getRipeWhiteAdminOptions = (environment, fallback) => {
+    switch (environment) {
+        case "ci":
+        case "test":
+            return {
+                baseUrl: "https://ripe-white-admin-ci.platforme.com/"
+            };
+        case "now":
+        case "now-branch":
+            return {
+                baseUrl: "https://ripe-white-admin-sbx.platforme.com/"
+            };
+        case "sandbox":
+            return {
+                baseUrl: "https://ripe-white-admin-sbx.platforme.com/"
+            };
+        case "stage":
+            return {
+                baseUrl: "https://ripe-white-admin-stage.platforme.com/"
+            };
+        case "production":
+            return {
+                baseUrl: "https://white-admin.platforme.com/"
+            };
+        default:
+            return fallback;
+    }
+};
+
+export const getRipeWhiteOptions = (environment, fallback) => {
+    switch (environment) {
+        case "ci":
+        case "test":
+            return {
+                baseUrl: "https://ripe-white-ci.platforme.com/"
+            };
+        case "now":
+        case "now-branch":
+            return {
+                baseUrl: "https://ripe-white-now.platforme.com/"
+            };
+        case "sandbox":
+            return {
+                baseUrl: "https://ripe-white-sbx.platforme.com/"
+            };
+        case "stage":
+            return {
+                baseUrl: "https://ripe-white-stage.platforme.com/"
+            };
+        case "production":
+            return {
+                baseUrl: "https://ripe-white.platforme.com/"
             };
         default:
             return fallback;
