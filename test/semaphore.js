b1be5429a09fdd123afd9e3f5342fe52b39dc396
Margarida Silva <ms@platforme.com>
2022-06-03 15:54:24 +0100

    test: semaphore with 2 requests

--- test/semaphore.js
+++ test/semaphore.js
@@ -18,6 +18,21 @@ describe("Semaphore", function() {
             assert.strictEqual(execution[0] < execution[1], true);
         });
 
+        it("should be able to execute two tasks two at a time", async () => {
+            const semaphore = ripeCommons.semaphore(2);
+
+            const execution = [];
+            const callback = async () => {
+                await new Promise(resolve => setTimeout(resolve, 10));
+                execution.push(Date.now());
+            };
+
+            await Promise.all([semaphore.execute(callback), semaphore.execute(callback)]);
+
+            assert.strictEqual(execution.length, 2);
+            assert.strictEqual(execution[0] - execution[1] < 10, true);
+        });
+
         it("should be able to execute two tasks one at a time, even if they fail", async () => {
             const semaphore = ripeCommons.semaphore();
 
